6d7c29299ef16373e748183c4333e580051d0bb1
fateme parviz <fatemeprz1999@gmail.com>
2024-10-28 14:34:43 +0330

    the logic of authorizatation function changed and notfound page was created

--- utils/authorization.js
+++ utils/authorization.js
@@ -4,11 +4,14 @@ const authHandeler=()=>{
 
     const cookie=getCookie()
     const url=location.href
-    
-    if((cookie && url.includes("auth")) ||
-    (!cookie && url.includes("dashboard"))){
-        location.assign("../index.html")
+
+    if(cookie && url.includes("auth")){
+        location.assign("./notfound.html")
         return false
+    }else if(!cookie && url.includes("dashboard")){
+        location.assign("./auth.html")
+        return false
+
     }
    
 }
