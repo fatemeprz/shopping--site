3c65ca532355b53ec8a1de66f646290b345d227f
fateme parviz <fatemeprz1999@gmail.com>
2024-11-05 14:06:45 +0330

    product cards developed and user can show them , card styles added

--- index.js
+++ index.js
@@ -1,30 +1,90 @@
+import { getCookie } from "./utils/cookie.js"
+
 const cards=document.querySelector(".cards")
-const loader=document.getElementById("loader")
+const loader=document.querySelector(".loader")
+const userButton=document.getElementById("user")
+const carts=document.querySelector(".carts")
 
 const BASE_URL="https://fakestoreapi.com"
 
-const getData=()=>{
+const getData=async()=>{
 
-    // try{
+    showUserButton()
 
-    //     fetch(`${BASE_URL}/products`)
-    //         .then(res=>res.json())
-    //         .then(data=>console.log(data))
-    //     start()
-        
-        
-    // }
-    // catch{
+    try{
 
-    // }
+        const res= await fetch(`${BASE_URL}/products`)
+        const data=await res.json()
+        console.log(data);
+        start()
+        showProducts(data)
+    }
+    catch{
+
+    }
     
     
+}
+
+const showProducts=async(data)=>{
+
+    // loader.style.display="none"
+    console.dir(loader.style.display);
+    const products=await data.forEach(item => {
+        createCart(item)
+         
+    });
+    
+}
+const createCart=(product)=>{
+    // console.log(product)
+    
+    const {image,title,price,rating:{count,rate},cateqory,id,description}= product
+    const productTitle=title.split(" ").slice(0,4).join(" ")
+  
+    const cartJSX=`
+    <div class="product">
+        <img class="product-img" src=${image}>
+        <div class="product-title">${productTitle}</div>
+        <div class="buy-section">
+            <div class="buy-section-right">$ ${price}</div>
+            <div class="buy-section-left">Buy <i class="fa-solid fa-cart-shopping icon"></i></div>
+        </div>
+        <div class="rating">
+            <div class="rating-right"><i class="fa-solid fa-star icon"></i>${rate}</div>
+            <div class="rating-left"><i class="fa-solid fa-user icon"></i>${count}</div>
+        </div>
+
+    </div>
+    `
+    carts.innerHTML+=cartJSX
+    
+
 }
 
 const start=()=>{
     loader.style.display="none"
     
 }
+
+const showUserButton=()=>{
+    const cookie=getCookie()
+
+    if(cookie){
+        userButton.innerHTML=`
+        <i class="fa-solid fa-house-chimney icon"></i>Dashboard
+        `
+        userButton.href="./public/dashboard.html"
+    }else{
+        userButton.innerHTML=`
+        <i class="fa-solid fa-arrow-right-to-bracket icon"></i>Login
+        `
+        userButton.href="./public/auth.html"
+    }
+
+}
+
+
 
 
 window.addEventListener("DOMContentLoaded",getData)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}",
-    './index.html',
-    './index.js'],
+  mode: "jit",
+  content: ["./public/**/*.{html,js}", "./index.html", "./index.js"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         primary: "#2465f7",
         secondary: "#1e66f72a",
         background: "#f8f8f8",
         warning: "#e9c601",
         error: "#f75e4b",
       },
-      space:{
-        88:'22rem'
+      space: {
+        88: "22rem",
       },
-      maxWidth:{
-        maxScreen:'1200px',
-      }
-
+      spacing: {
+        88: "22rem",
+        90: "22.5rem",
+      },
+      maxWidth: {
+        maxScreen: "1200px",
+      },
+      gridTemplateColumns: {
+        "auto-fill-56": "repeat(auto-fill, minmax(224px, 1fr))",
+        "auto-fit-56": "repeat(auto-fit, minmax(224px, 1fr))",
+      },
+      gridTemplateRows: {
+        "auto-fill-90": "repeat(auto-fill, minmax(360px, 1fr))",
+        "auto-fit-90": "repeat(auto-fit, minmax(360px, 1fr))",
+      },
     },
   },
   plugins: [],
   variants: {
     extend: {
-      scale: ["group-hover"]
+      scale: ["group-hover"],
     },
   },
-}
-
+};
